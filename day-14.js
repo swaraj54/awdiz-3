01618dbf0e4a54bcacac0c79c4b4ba5229b8476d
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-05-25 10:52:16 +0530

    Updates

---
+++ day-14.js
@@ -0,0 +1,25 @@
+Earth - solid, liquid, gas 
+Js- datatypes , String , Number, Boolean 
+
+String - text - '1234' , "LENOVO", "Hello" , `Hi`
+Number - number - 123, 34,65.56,7889856,0.344
+Boolean - true , false 
+
+Varibles - we can store the data - 
+Sntax = 
+var kuchbhi = 'Awdiz';
+console.log(kuchbhi);    
+
+var kuchbhi = '12345';
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
+var kuchbhi = 12345;
+console.log("Data here-", kuchbhi)
+console.log("Datatype -", typeof(kuchbhi));
+
+var booleanValue = 'false';
+console.log(booleanValue)
+console.log(typeof (booleanValue))
+var booleanValue = false;
+console.log(booleanValue)
+console.log(typeof (booleanValue))
