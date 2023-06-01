890e7927347e608d996e279e4859b6c2341b3950
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-06-01 10:40:20 +0530

    Updates for question

--- Day-18.js
+++ Day-18.js
@@ -27,3 +27,17 @@ for (var i = 0; i < array.length; i++) {
         console.log(array[i], " + ", array[j], " = " ,array[i] + array[j])
     }
 }
+
+
+// Q Given an target find two number from array which subtration is target . 
+
+var target = 93;
+var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7];
+// 100,7
+
+// Q Given an target find threee number from array which additionation is target . 
+
+var target = 93;
+var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7, 91,1,1];
+// 91,1,1
+// Hint - use three nested for loops
