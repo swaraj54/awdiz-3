ed92105392abbf18b30aa3c80362822e379fdca1
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-06-10 15:33:07 +0530

    updates

--- DAY-24.js
+++ DAY-24.js
@@ -9,6 +9,18 @@ function register(event) {
     var password = document.getElementById("userPassword").value;
     console.log(password, "password")
 
+    const userData = {
+        username: name,
+        usernumber: number,
+        useremail: email,
+        userpassword: password
+    }
+
+    localStorage.setItem("StudentsList", JSON.stringify(userData))
+
+
+
+
     document.getElementById("userName").value = ""; // assigning
     document.getElementById("userNumber").value = "";
     document.getElementById("userEmail").value = "";
@@ -20,4 +32,56 @@ function register(event) {
 
 
 
+// var dataFromLS = JSON.parse(localStorage.getItem("StudentsList"));
+// localStorage.setItem("stundets", "[ 1,2,4,5]");
+// localStorage.removeItem("stundets")
+
+
+
 // Native JavaScript
+
+
+
+// var studentsList = [ { name: "Swaraj" , email : "abc@.gsam.com" },
+//                     { name: 'AVinahs', email : "abc@.gsam.com" },
+//                     { name: " PrATIK", email : "abc@.gsam.com" } ]
+
+
+
+
+// var array = [2, 4354, 65, 56, 7, "3245", "IJHUGY"];
+
+
+// var myData = {
+//     myAge: 50,
+//     mySurName: "Jadhav",
+//     xyz: true,
+//     myHYIK: [1, 23, "ada", awd, awd],
+//     myHello: { hello1: [2, 3, 454, 7] }
+// }
+// console.log(myData.myAge)50
+// console.log(myData.mySurName)Jadhav
+// console.log(myData.xyz) true
+
+
+
+// JS
+// JSON -
+
+
+// { name: 'Swaraj' }
+
+// { "name": "Swaraj" }
+
+// JSON.stringify(DATA) => JS TO JSON
+// JSON.parse(DATA) => JSON TO JS
+
+
+login 
+register 
+home 
+mutliple, men, women , kidsa, 6
+single page,
+profile,
+cart 
+wishlist
