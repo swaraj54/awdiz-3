4cffb2405ffc4b70a2bb9a89412ee7fe67576ca0
Swaraj Jadhav <70018714+swaraj54@users.noreply.github.com>
2023-06-11 12:55:40 +0530

    Queston

--- day-25.js
+++ day-25.js
@@ -31,23 +31,57 @@
 
 // Q. Find the numbers who divisible by 5 from given range and return the array of those number... 
 
-var start = 9876;
-var end = 10234;
+// var start = 9876;
+// var end = 10234;
 
-function diviBY5(start, end) {
-    var result = [];
-    for (var i = start; i <= end; i++) {
-        if (i % 5 == 0) {
-            result.push(i)
-            // console.log(i)
+// function diviBY5(start, end) {
+//     var result = [];
+//     for (var i = start; i <= end; i++) {
+//         if (i % 5 == 0) {
+//             result.push(i)
+//             // console.log(i)
+//         }
+//     }
+//     console.log(result,"- res")
+// }
+// diviBY5(start, end)
+
+
+
+var givenArray = [23, 45, 56, 67, 23, 56, 68, 34, 4556, 78, 655, 23, 568787, 89845, 343, 54576, 7873, 323, 54, 636, 6836, 575];
+
+// find numbers who odd from given array and return array with those number... 
+
+
+
+function helo(array) {
+
+    var oddNumberArray = [];
+
+    for (var i = 0; i < array.length; i++) {
+        // console.log(array[i])
+        if (array[i] % 2 == 1) {
+            oddNumberArray.push(array[i])
         }
     }
-    console.log(result,"- res")
+
+    console.log(oddNumberArray, "- oddNumberArray")
+
 }
-diviBY5(start, end)
+helo(givenArray);
 
 
+Q.Find numbers who are divisible by 2, 5, and 10 from given range,
+    then use that array to make new array with second element of array.
+(note - use reverse for loop)
 
-var givenArray = [23,45,56,67,23,56,68,34,4556,78,655,23,568787,89845,343,54576,7873,323,54,636,6836,575];
+    var start = 8723;
+var end = 6567;
+// 2, 5, and 10
 
-find numbers who odd from given array and return array with those number... 
+function twoStage() { }
+
+
+var filteredArray = [100, 110, 120, 130, 140, 150]  //  2, 5, and 10 = 100
+
+var result = [110, 130, 150]
